24c5b75015ebf8a1d7158fe91e9c80e600ef8c2a
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-03-28 14:25:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+
+      <h1 className="text-6xl text-red-500">hello react</h1>
+    </>
+  );
+}
+
+export default App;
